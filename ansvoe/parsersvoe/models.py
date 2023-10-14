3d5110f61e0bf18634c7aa8b1c6877f36167c66a
goose <sumich-tv@yandex.ru>
2023-10-14 21:17:10 +0700

    feat: add new fields to models

--- ansvoe/parsersvoe/models.py
+++ ansvoe/parsersvoe/models.py
@@ -4,4 +4,18 @@ from django.db import models
 # Create your models here.
 class Apartments(models.Model):
     id_crm = models.CharField(max_length=4)
+    price = models.CharField(max_length=255)
+    area = models.CharField(max_length=5, default='null')
+    area_living = models.CharField(max_length=5, default='null')
+    area_kitchen = models.CharField(max_length=5, default='null')
+    floor = models.CharField(max_length=2)
+    floor_total = models.CharField(max_length=2)
+    building = models.CharField(max_length=50)
+    bathroom = models.CharField(max_length=4)
+    balcony = models.CharField(max_length=20)
+    build_year = models.CharField(max_length=4)
+    description = models.CharField(max_length=255)
+    property_type = models.CharField(max_length=255)
     rooms = models.CharField(max_length=2)
+    house = models.CharField(max_length=4)
+    street = models.CharField(max_length=255)
